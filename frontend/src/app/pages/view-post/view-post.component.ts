ba3346e9be2cf8b420173f6c0ffe3cd0dca3e11c
Jaya Krishna <jayamannem07@gmail.com>
2024-08-21 11:19:14 +0530

    Refactor ViewPostComponent to use IncomingPost type for postData

--- frontend/src/app/pages/view-post/view-post.component.ts
+++ frontend/src/app/pages/view-post/view-post.component.ts
@@ -11,32 +11,41 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { CommentService } from '../../services/comment.service';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
+import { IncomingPost } from '../../models/incoming-post.model';
+
+
 
 @Component({
   selector: 'app-view-post',
   standalone: true,
-  imports: [MatCard, MatCardHeader, MatCardAvatar, MatCardTitle, MatCardSubtitle, MatCardImage, MatCardContent,
-            MatChipSet, MatChip, MatCardActions, MatButtonModule, MatIcon,NgFor, DatePipe, ReactiveFormsModule, MatFormField, 
-            MatLabel, MatInput],
+  imports: [
+    MatCard, MatCardHeader, MatCardAvatar, MatCardTitle, MatCardSubtitle, MatCardImage, MatCardContent,
+    MatChipSet, MatChip, MatCardActions, MatButtonModule, MatIcon, NgFor, DatePipe, ReactiveFormsModule, MatFormField,
+    MatLabel, MatInput
+  ],
   templateUrl: './view-post.component.html',
-  styleUrl: './view-post.component.css'
+  styleUrls: ['./view-post.component.css'] // Corrected styleUrl to styleUrls
 })
 export class ViewPostComponent {
   postId!: number;
-  postData!: any;
-  comments!: any;
+  postData!: IncomingPost; // Changed to IncomingPost
+  comments!: any; // This can remain as any or be strongly typed if a Comment interface is defined
 
   commentForm!: FormGroup;
 
-  constructor(private postService: PostService, private commentService: CommentService,
-               private snackBar: MatSnackBar, 
-              private activatedRoute: ActivatedRoute, private fb: FormBuilder){
-  }
-  ngOnInit(){
+  constructor(
+    private postService: PostService,
+    private commentService: CommentService,
+    private snackBar: MatSnackBar,
+    private activatedRoute: ActivatedRoute,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit() {
     this.activatedRoute.params.subscribe(params => {
-      this.postId = +params['id'];  // Use the '+' sign to convert the string to a number
+      this.postId = +params['id'];
     });
-    console.log(`in view-postcomponet.ts, initiating postId:  ${this.postId}`);
+    console.log(`in view-post.component.ts, initiating postId: ${this.postId}`);
     this.getPostById();
 
     this.commentForm = this.fb.group({
@@ -45,61 +54,59 @@ export class ViewPostComponent {
     });
   }
 
-  getCommentsByPostId(){
+  getCommentsByPostId() {
     this.commentService.getCommentsByPost(this.postId).subscribe({
       next: (res) => {
         this.comments = res;
       },
       error: (error) => {
-        this.snackBar.open('something went wrong');
+        this.snackBar.open('Something went wrong');
         console.log(error);
       }
     });
   }
 
-  getPostById(){
+  getPostById() {
     this.postService.getPostById(this.postId).subscribe({
-      next: (res) => {
+      next: (res: IncomingPost) => { // Specify the type of res as IncomingPost
         this.postData = res;
         console.log(this.postData);
         this.getCommentsByPostId();
       },
       error: (error) => {
-        this.snackBar.open(`There was an error getting the post`, 'Error', {duration: 3000});
+        this.snackBar.open('There was an error getting the post', 'Error', { duration: 3000 });
         console.log(error);
       }
     });
   }
 
-  likePost(){
+  likePost() {
     this.postService.likePost(this.postId).subscribe({
       next: (res) => {
         this.postData.likeCount += 1;
-        this.snackBar.open('post liked successfully');
+        this.snackBar.open('Post liked successfully');
         console.log(res);
       },
       error: (error) => {
-        this.snackBar.open(`something went wrong`);
+        this.snackBar.open('Something went wrong');
         console.log(error);
       }
     });
   }
 
-  publicComment(){
+  publicComment() {
     const postedBy = this.commentForm.get('postedBy')?.value;
     const content = this.commentForm.get('content')?.value;
 
     this.commentService.createComment(this.postId, postedBy, content).subscribe({
       next: (res) => {
-        this.snackBar.open('comment successfully published');
+        this.snackBar.open('Comment successfully published');
         this.getCommentsByPostId();
       },
       error: (error) => {
-        this.snackBar.open('something went wrong', 'error');
+        this.snackBar.open('Something went wrong', 'Error');
         console.log(error);
       }
     });
   }
-
-  
 }
